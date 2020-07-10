db49b04db21f21391938a3ff39167892cc6f921f
Ashley Holanda <aholanda12@gmail.com>
2020-07-10 16:55:02 -0400

    added all appended elements of city's current weather including uv

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,10 @@ function populateCityWeather() {
     $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response) {
+      }).then(function(weather) {
 
         console.log(queryURL);
+        console.log(weather);
 
         var nowMoment = moment();
 
@@ -17,6 +18,55 @@ function populateCityWeather() {
         $("#city-name").append(
           displayMoment.text("(" + nowMoment.format("M/D/YYYY") + ")")
         );
+
+        var cityName = $("<h2>").text(weather.name);
+        $("#city-name").prepend(cityName);  
+
+        var weatherIcon = $("<img>");
+        weatherIcon.attr(
+            "src",
+            "https://openweathermap.org/img/w/" + weather.weather[0].icon + ".png"
+        );
+
+        $("#current-icon").empty();
+        $("#current-icon").append(weatherIcon);
+
+        $("#current-temp").text("Temperature: " + weather.main.temp + " °F");
+        $("#current-humidity").text("Humidity: " + weather.main.humidity + "%");
+        $("#current-wind").text("Wind Speed: " + weather.wind.speed + " MPH");
+        
+        latitude = weather.coord.lat;
+        longitude = weather.coord.lon;
+  
+        var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=9bcf5e17bdf9a0b802d15f96847a6ef0&lat=" + latitude + "&lon=" + longitude;
+  
+        $.ajax({
+            url: queryURL2,
+            method: "GET"
+          }).then(function(uvIndex) {
+            console.log(uvIndex);
+    
+            var uvIndexDisplay = $("<button>");
+            uvIndexDisplay.addClass("btn");
+            if (uvIndex.value <= 2) {
+                uvIndexDisplay.addClass("btn-success");
+            }
+            else if (uvIndex.value > 2 && uvIndex.value < 9) {
+                uvIndexDisplay.addClass("btn-warning");
+            }
+            else if (uvIndex.value >= 9) {
+                uvIndexDisplay.addClass("btn-danger");
+            }
+    
+            $("#current-uv").text("UV Index: ");
+            $("#current-uv").append(uvIndexDisplay.text(uvIndex.value));
+            console.log(uvIndex.value);
+          });
+
+
+
+
+
     });
     
     
